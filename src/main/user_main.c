ff73e7c73fc3068edef8055f78e6053eb35f77d2
claudiosilva <claudioccdsilva@gmail.com>
2023-02-26 01:04:45 -0300

    modificado a placa

--- src/main/user_main.c
+++ src/main/user_main.c
@@ -1,5 +1,5 @@
 #include <string.h>
-// #include "freertos/FreeRTOS.h"
+#include "freertos/FreeRTOS.h"
 // #include "freertos/task.h"
 #include "esp_system.h"
 // #include "esp_wifi.h"
@@ -7,38 +7,7 @@
 // #include "lwip/err.h"
 // #include "lwip/sys.h"
 
-uint32 ICACHE_FLASH_ATTR
-user_rf_cal_sector_set(void)
-{
-    flash_size_map size_map = system_get_flash_size_map();
-    uint32 rf_cal_sec = 0;
 
-    switch (size_map) {
-        case FLASH_SIZE_4M_MAP_256_256:
-            rf_cal_sec = 128 - 8;
-            break;
-
-        case FLASH_SIZE_8M_MAP_512_512:
-            rf_cal_sec = 256 - 5;
-            break;
-
-        case FLASH_SIZE_16M_MAP_512_512:
-        case FLASH_SIZE_16M_MAP_1024_1024:
-            rf_cal_sec = 512 - 5;
-            break;
-
-        case FLASH_SIZE_32M_MAP_512_512:
-        case FLASH_SIZE_32M_MAP_1024_1024:
-            rf_cal_sec = 1024 - 5;
-            break;
-
-        default:
-            rf_cal_sec = 0;
-            break;
-    }
-
-    return rf_cal_sec;
-}
 
 void app_main()
 {
